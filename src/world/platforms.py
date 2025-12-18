5ccbd1248007917522e222ff0080acc5a49b9de0
CauaEmano <emcaua@gmail.com>
2025-12-18 10:14:11 -0300

    Espelhei o fundo kkkk

--- src/world/platforms.py
+++ src/world/platforms.py
@@ -5,10 +5,14 @@ class Background(pygame.sprite.Sprite):
         super().__init__()
         background = pygame.image.load('assets/images/cenario.png').convert() #Carreguei o fundo
         background = pygame.transform.scale(background, (1280, 850)) #moldei o tamanho
+        back_flipado = pygame.transform.flip(background, flip_x=True, flip_y=False)
 
         self.image = pygame.Surface((20000, background.get_height()), pygame.SRCALPHA)  #Criei uma superfície gigante
+        contador = 0
         for x in range(0, 20000, background.get_width()): #Iterei sobre a superfície gigante pra desenhar o fundo sobre ela
-            self.image.blit(background, (x, 0)) #Desenhando ne be
+            contador += 1
+            if contador%2 != 0: self.image.blit(background, (x, 0)) #Desenhando ne be
+            else: self.image.blit(back_flipado, (x, 0))
 
         self.rect = self.image.get_rect()
 
@@ -17,6 +21,7 @@ class Chao(pygame.sprite.Sprite):
         super().__init__()
 
         tile_img = pygame.image.load('assets/images/chao.png').convert_alpha()
+        tile_flipado = pygame.transform.flip(tile_img, flip_x=True, flip_y=False)
     
         largura_tile = tile_img.get_width()
         altura_tile = tile_img.get_height() 
@@ -24,9 +29,11 @@ class Chao(pygame.sprite.Sprite):
         largura_total_mundo = 20000
 
         self.image = pygame.Surface((largura_total_mundo, altura_tile), pygame.SRCALPHA) 
-
+        contador = 0
         for x in range(0, largura_total_mundo, largura_tile):
-            self.image.blit(tile_img, (x, 0))
+            contador += 1
+            if contador%2 != 0: self.image.blit(tile_img, (x, 0))
+            else: self.image.blit(tile_flipado, (x, 0))
 
         self.rect = self.image.get_rect(top=600, left=0)
 

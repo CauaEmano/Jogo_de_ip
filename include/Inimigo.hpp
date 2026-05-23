b114228704f5009ab3dcf9c444b4b26479fa3400
Sophia Sá <ssfs@cin.ufpe.br>
2026-05-22 21:55:29 -0300

    Adiciona classe abstrata Inimigo

---
+++ include/Inimigo.hpp
@@ -0,0 +1,33 @@
+#ifndef INIMIGO_H
+#define INIMIGO_H
+
+#include <string>
+
+class Inimigo {
+    private:
+        std::string nome;
+        double vida;
+        double dano; 
+        double posicao_x; //posições para inimigos no mapa
+        double posicao_y;
+        
+
+    public:
+        Inimigo(std::string n, double v, double d, double x, double y) : nome(n), vida(v), dano(d), posicao_x(x), posicao_y(y){}
+        virtual ~Inimigo() {}
+
+        //ações
+        virtual void atacar() =0; //método virtual puro, permite às classes filhas terem diferentes maneiras de atacar
+        void receberDano(int qtd); 
+        void mostrarStatus() const;
+
+        std::string getNome()const {return nome;};
+        double getVida() const {return vida;};
+        double getDano() const {return dano;};
+        double getPosicaoX() const {return posicao_x;};
+        double getPosicaoY() const {return posicao_y;};
+
+};
+
+
+#endif
